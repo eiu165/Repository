5b50f5de5af87a7f42ddd3525c7bc2c487cb7f30
eiu165 <eiu165@gmail.com>
2014-02-20 16:58:58 -0600

    added web project

---
+++ Source/Repository.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Repository.Web.Startup))]
+namespace Repository.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
